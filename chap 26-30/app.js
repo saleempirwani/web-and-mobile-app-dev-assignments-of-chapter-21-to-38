b741b3668f1158e59c1044c4a9dabc3f3d58313d
Muhammad Saleem Raza <salimraza3465@gmail.com>
2020-06-18 22:35:36 +0500

    done ch 26 - 30

---
+++ chap 26-30/app.js
@@ -0,0 +1,80 @@
+/*********** Chap 26 - 30 ************/
+
+// Task: 1 
+
+// var num = +prompt('Enter a positive no')
+// document.write(`
+//     Number: ${num} <br>
+//     Round Off: ${Math.round(num)} <br>
+//     Floor: ${Math.floor(num)} <br>
+//     Ceil: ${Math.ceil(num)}
+// `)
+
+
+
+// Task: 2
+
+// var num = +prompt('Enter a negative no')
+// document.write(`
+//     Number: ${-num} <br>
+//     Round Off: ${-Math.round(num)} <br>
+//     Floor: ${-Math.floor(num)} <br>
+//     Ceil: ${-Math.ceil(num)}
+// `)
+
+
+// Task: 3
+
+// var num = +prompt('Enter a negative no')
+// document.write(`
+//     The absolute value of ${num} is ${Math.abs(num)}
+// `)
+
+
+// Task: 4
+
+// document.write(`
+//     Random Dice:  ${Math.round(Math.random() * 6) }
+// `)
+
+
+// Task: 5
+
+// toss_value = Math.round(Math.random())
+// toss = ''
+
+// if (toss_value === 0) {
+//     toss = 'Heads'
+// }
+// else {
+//     toss = 'Tails'
+// }
+
+// document.write(`Random Coin Value: ${toss}`)
+
+
+// Task: 6
+
+// document.write(`
+//     Random number between 1 and 100:  ${Math.round(Math.random() * 99) + 1 }
+// `)
+
+
+// Task: 7
+
+// var weight = prompt('Enter your weight')
+// weight_amount = weight.match('([0-9][0-9].[0-9]|[0-9][0-9])')[0]
+// document.write(`The weight of user is:  ${weight_amount} kilograms`)
+
+
+// Task: 8
+
+// var random_num = Math.round(Math.random() * 9) + 1
+// var user_num = +prompt('Enter a number betweem (1 - 10) ')
+// if (random_num === user_num){
+//     alert('Congrates! you won')
+// }
+// else{
+//     alert(`Try Again! it was ${random_num}`)
+// }
+
